00f57f11d319e3b331058dbb4f7c874cf4bb0915
Ashish <Ashish@192.168.8.72>
2021-12-18 13:56:54 +0530

    Sprin Boot swagger configuration

---
+++ springboot-swagger2/src/main/java/com/springbootapi/swagger2/SpringbootSwagger2Application.java
@@ -0,0 +1,16 @@
+package com.springbootapi.swagger2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+public class SpringbootSwagger2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSwagger2Application.class, args);
+	}
+	
+	
+}
